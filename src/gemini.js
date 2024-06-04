a5458ae5d8d8dd6b0c7e288ff5a3563750da5991
Agustin-Gonzalorena <agonzalorena1@gmail.com>
2024-06-04 11:49:34 -0300

    update gemini settings

--- src/gemini.js
+++ src/gemini.js
@@ -1,7 +1,11 @@
 import { config } from "dotenv";
 config();
 
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import {
+  GoogleGenerativeAI,
+  HarmBlockThreshold,
+  HarmCategory,
+} from "@google/generative-ai";
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
@@ -13,9 +17,13 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 // ...
 export async function consultGemini(stringDate) {
   const prompt = `Dame una unica efemeride, de forma breve(10 reglones maximo), del dia ${stringDate}`;
+  const safetySettings = {
+    HarmCategory: HarmCategory.NONE,
+    HarmBlockThreshold: HarmBlockThreshold.NONE,
+  };
+  const result = await model.generateContent(prompt, safetySettings);
+  const response = result.response;
 
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
   const text = response.text();
   console.log(text);
   const text_ = checkText(text);
